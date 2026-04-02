7792c8392b0938ea9a552ab64c84727a364b512f
Aditya <aditya07.11.04.12@gmail.com>
2026-04-03 01:27:39 +0530

    Added and configured dependencies to allow usage of DTO Models in API Gateway

--- API_GATEWAY/API_GATEWAY/src/main/java/com/example/API_GATEWAY/Services.java
+++ API_GATEWAY/API_GATEWAY/src/main/java/com/example/API_GATEWAY/Services.java
@@ -2,6 +2,10 @@ package com.example.API_GATEWAY;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.example.Payload;
+import org.example.ProcessorResponse;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 public class Services {
@@ -9,6 +13,9 @@ public class Services {
     @PostMapping("/process")
     public ProcessorResponse processRequest(Payload payload)
     {
-        return payload.ProcessResponse();
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        charCountMap.put('a', 5);
+        ProcessorResponse response = new ProcessorResponse(charCountMap);
+        return response;
     }
 }
